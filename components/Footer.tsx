6479a3a77e48f305b8d80b5bb91159b7d06e1c73
Glory <okewoleglory@gmail.com>
2023-09-18 16:40:05 +0100

    feat: I implemented footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,25 +1,25 @@
 import React from 'react'
-import logo from '../public/logo.png';
-import Image from 'next/image';
-// import styles from '../styles/Auth.module.scss';
 import Link from 'next/link';
 
 const Footer = () => {
     return (
         <>
             <footer>
-                <div className="py-3">
-                    <div className="row justify-content-between">
-                        <div className="col-md-3">
-                            Wolff Olins 2023
-                        </div>
-                        <div className="col-md-5">
-                            
+                <div className="row justify-content-between align-items-between container-fluid py-3">
+                    <div className="col-md-3">
+                        Wolff Olins 2023
+                    </div>
+                    <div className="row justify-content-center col-md-6">
+                        <div className='col-md-3'>Cookie Policy</div>
+                        <div className='col-md-3'>Privacy notice</div>
+                        <div className="col-md-4 d-flex align-items-center">
+                            <Link href=''><div className={`footer-icon me-4 text-center `}><i className="fa fa-instagram" aria-hidden="true"></i></div></Link>
+                            <Link href=''><div className={`footer-icon text-center me-4`}><i className="fa fa-twitter" aria-hidden="true"></i></div></Link>
+                            <Link href=''><div className={`footer-icon text-center me-4`}><i className="fa fa-youtube" aria-hidden="true"></i></div></Link>
+                            <Link href=''><div className={`footer-icon text-center me-4`}><i className="fa fa-linkedin" aria-hidden="true"></i></div></Link>
                         </div>
                     </div>
                 </div>
-                {/* <div className="foote">
-                </div> */}
             </footer>
         </>
     )
